da46c6ae6a4587e85e379fd3c0038ee37b4c7e04
0ut0f4mm0 <eriking2004@gmail.com>
2023-05-15 10:58:40 +0200

    minor changes to work with dictionaries

--- main.py
+++ main.py
@@ -4,62 +4,60 @@ import random
 app = Flask(__name__)
 app.secret_key = 'mysecretkey'
 
-# Define the number of digits in the code
-code_length = 4
+secret_length = 4
 
-# Define the maximum number of attempts
 max_attempts = 10
 
-# Define the colors available for the code and the guesses
 colors = ['red', 'green', 'blue', 'yellow', 'orange', 'purple']
+colors_dict = {'red' : "#ff0000",
+               'blue' : "#0000ff",}
 
-def generate_code():
-    #Generate a random code.
-    return [random.choice(colors) for _ in range(code_length)]
+def generate_secret():
+    #Generate a random secret.
+    return [random.choice(colors) for _ in range(secret_length)]
 
 
-def check_guess(code, guess):
-    """Check the guess against the code and return the number of correct colors and positions
+def check_guess(secret, guess):
+    """Check the guess against the secret and return the number of correct colors and positions
     and the number of correct colors but incorrect positions."""
     correct_positions = 0
     correct_colors = 0
-    code = code.copy()
+    secret = secret.copy()
     guess = guess.copy()
-    for i in range(code_length):
-        if guess[i] == code[i]:
+    for i in range(secret_length):
+        if guess[i] == secret[i]:
             correct_positions += 1
-    for i in range(code_length):
-        if guess[i] is not None and guess[i] in code:
+    for i in range(secret_length):
+        if guess[i] is not None and guess[i] in secret:
             correct_colors += 1
-            code[code.index(guess[i])] = None
+            secret[secret.index(guess[i])] = None
             guess[i] = None
     return correct_positions, correct_colors
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    #Display the game board and handle the user's guesses.
-    if 'code' not in session:
-        session['code'] = generate_code()
+    if 'secret' not in session:
+        session['secret'] = generate_secret()
         session['attempts'] = 0
         session['history'] = []
     if request.method == 'POST':
         guess = request.form.getlist('guess[]')
         if guess:
             session['attempts'] += 1
-            guess_result = check_guess(session['code'], guess)
+            guess_result = check_guess(session['secret'], guess)
             session['history'].append((guess, guess_result))
-            if guess_result[0] == code_length:
+            if guess_result[0] == secret_length:
                 new_game()
                 return render_template('win.html', attempts=session['attempts'])
             elif session['attempts'] >= max_attempts:
                 new_game()
                 return render_template('lose.html')
-    return render_template('game.html', colors=colors, history=session['history'], max_attempts=max_attempts)
+    return render_template('game.html', colors=colors_dict, history=session['history'], max_attempts=max_attempts)
 
 def new_game():
     # Generate new secret and rest session variables
-    session['code'] = generate_code()
+    session['secret'] = generate_secret()
     session['attempts'] = 0
     session['history'] = []
 
